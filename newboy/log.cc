0a19b7d2c095c430e00eeb645b9d2a6770500810
dejavudwh <daweihucs@outlook.com>
2021-07-05 01:54:31 +0800

    test log pass

--- newboy/log.cc
+++ newboy/log.cc
@@ -136,6 +136,8 @@ void FileLogAppender::log(std::shared_ptr<Logger> logger, LogLevel::Level level,
 
         if(!m_formatter->format(m_filestream, logger, level, event)) {
             std::cout << "error" << std::endl;
+        } else {
+            m_filestream.close();
         }
     }
 }
@@ -145,13 +147,16 @@ bool FileLogAppender::reopen() {
         m_filestream.close();
     }
 
-    m_filestream.open(m_filename);
-    
-    return !!m_filestream;
+    m_filestream.open(m_filename.c_str(), std::ios::app);   
+    if(!m_filestream.is_open()) {
+        m_filestream.open(m_filename.c_str(), std::ios::app);
+    }
+
+    return m_filestream.is_open();
 }
 
 void StdoutLogAppender::log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) {
-    if (level > m_level) {
+    if (level >= m_level) {
         std::cout << m_formatter->format(logger, level, event);
     }
 }
@@ -330,58 +335,55 @@ class TabFormatItem : public LogFormatter::FormatItem {
         std::string m_string;
 };
 
-//%xxx %x{xxx} %%
+//%xxx %xxx{xxx} %%
 void LogFormatter::init() {
-    // str format type
+    //str, format, type
     std::vector<std::tuple<std::string, std::string, int> > vec;
     std::string nstr;
-    size_t pattern_size = m_pattern.size();
-    for (size_t i = 0; i < pattern_size; i++) {
-        // 纯字符串
-        if (m_pattern[i] != '%') {
+    for(size_t i = 0; i < m_pattern.size(); ++i) {
+        if(m_pattern[i] != '%') {
             nstr.append(1, m_pattern[i]);
             continue;
         }
 
-        // %%
-        if (((i + 1) < pattern_size) && m_pattern[i + 1] == '%') {
-            nstr.append(1, '%');
-            continue;
+        if((i + 1) < m_pattern.size()) {
+            if(m_pattern[i + 1] == '%') {
+                nstr.append(1, '%');
+                continue;
+            }
         }
 
-        // format
         size_t n = i + 1;
         int fmt_status = 0;
         size_t fmt_begin = 0;
+
         std::string str;
         std::string fmt;
-
-        // find fmt
-        while (n < pattern_size) {
-            if (!fmt_status && (!isalpha(m_pattern[i]))
-                && m_pattern[n] != '{' && m_pattern[n] != '}') {
+        while(n < m_pattern.size()) {
+            if(!fmt_status && (!isalpha(m_pattern[n]) && m_pattern[n] != '{'
+                    && m_pattern[n] != '}')) {
                 str = m_pattern.substr(i + 1, n - i - 1);
                 break;
             }
-
-            if (fmt_status == 0) {
-                // format begin
-                if (m_pattern[n] == '{') {
+            if(fmt_status == 0) {
+                if(m_pattern[n] == '{') {
                     str = m_pattern.substr(i + 1, n - i - 1);
+                    //std::cout << "*" << str << std::endl;
                     fmt_status = 1; //解析格式
                     fmt_begin = n;
                     ++n;
                     continue;
                 }
-            } else if (fmt_status == 1) {
+            } else if(fmt_status == 1) {
                 if(m_pattern[n] == '}') {
                     fmt = m_pattern.substr(fmt_begin + 1, n - fmt_begin - 1);
+                    //std::cout << "#" << fmt << std::endl;
                     fmt_status = 0;
                     ++n;
                     break;
                 }
             }
-
+            
             ++n;
             if(n == m_pattern.size()) {
                 if(str.empty()) {
@@ -390,15 +392,16 @@ void LogFormatter::init() {
             }
         }
 
-        if (fmt_status == 0) {
-            if (!nstr.empty()) {
+        if(fmt_status == 0) {
+            if(!nstr.empty()) {
                 vec.push_back(std::make_tuple(nstr, std::string(), 0));
                 nstr.clear();
             }
             vec.push_back(std::make_tuple(str, fmt, 1));
             i = n - 1;
-        } else if (fmt_status == 1) {
+        } else if(fmt_status == 1) {
             std::cout << "pattern parse error: " << m_pattern << " - " << m_pattern.substr(i) << std::endl;
+            m_error = true;
             vec.push_back(std::make_tuple("<<pattern_error>>", fmt, 0));
         }
     }
@@ -408,35 +411,36 @@ void LogFormatter::init() {
     }
     static std::map<std::string, std::function<FormatItem::ptr(const std::string& str)> > s_format_items = {
         #define XX(str, C) \
-            {#str, [](const std::string& fmt) { return FormatItem::ptr(new C(fmt));}}
+                {#str, [](const std::string& fmt) { return FormatItem::ptr(new C(fmt));}}
 
-            XX(m, MessageFormatItem),           //m:消息
-            XX(p, LevelFormatItem),             //p:日志级别
-            XX(r, ElapseFormatItem),            //r:累计毫秒数
-            XX(c, NameFormatItem),              //c:日志名称
-            XX(t, ThreadIdFormatItem),          //t:线程id
-            XX(n, NewLineFormatItem),           //n:换行
-            XX(d, DateTimeFormatItem),          //d:时间
-            XX(f, FilenameFormatItem),          //f:文件名
-            XX(l, LineFormatItem),              //l:行号
-            XX(T, TabFormatItem),               //T:Tab
-            XX(F, FiberIdFormatItem),           //F:协程id
-            XX(N, ThreadNameFormatItem),        //N:线程名称
+                XX(m, MessageFormatItem),           //m:消息
+                XX(p, LevelFormatItem),             //p:日志级别
+                XX(r, ElapseFormatItem),            //r:累计毫秒数
+                XX(c, NameFormatItem),              //c:日志名称
+                XX(t, ThreadIdFormatItem),          //t:线程id
+                XX(n, NewLineFormatItem),           //n:换行
+                XX(d, DateTimeFormatItem),          //d:时间
+                XX(f, FilenameFormatItem),          //f:文件名
+                XX(l, LineFormatItem),              //l:行号
+                XX(T, TabFormatItem),               //T:Tab
+                XX(F, FiberIdFormatItem),           //F:协程id
+                XX(N, ThreadNameFormatItem),        //N:线程名称
         #undef XX
     };
 
-    for (auto& i : vec) {
-        // nstr
+    for(auto& i : vec) {
         if(std::get<2>(i) == 0) {
             m_items.push_back(FormatItem::ptr(new StringFormatItem(std::get<0>(i))));
         } else {
             auto it = s_format_items.find(std::get<0>(i));
             if(it == s_format_items.end()) {
                 m_items.push_back(FormatItem::ptr(new StringFormatItem("<<error_format %" + std::get<0>(i) + ">>")));
+                m_error = true;
             } else {
                 m_items.push_back(it->second(std::get<1>(i)));
             }
         }
+
     }
 }
 
--- tests/test_log.cc
+++ tests/test_log.cc
@@ -6,11 +6,13 @@ int main(int argc, char const *argv[]) {
     std::cout << "Hello Newboy Logger!" << std::endl;
 
     newboy::Logger::ptr logger(new newboy::Logger);
+    newboy::LogFormatter::ptr fmt(new newboy::LogFormatter("%d%T%p%T%c%T%f%T%l"));
 
-    logger->addAppender(newboy::LogAppender::ptr(new newboy::StdoutLogAppender));
+    newboy::StdoutLogAppender::ptr stdout_appender(new newboy::StdoutLogAppender);
+    stdout_appender->setFormatter(fmt);
+    logger->addAppender(stdout_appender);
 
     newboy::FileLogAppender::ptr file_appender(new newboy::FileLogAppender("./log.txt"));
-    newboy::LogFormatter::ptr fmt(new newboy::LogFormatter("%d%T%p%T%m%n"));
     file_appender->setFormatter(fmt);
     file_appender->setLevel(newboy::LogLevel::ERROR);
 
