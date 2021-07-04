e7ed3c27f05e8ada69005e25ae099ff9314b43f7
dejavudwh <daweihucs@outlook.com>
2021-07-04 18:18:12 +0800

    add test_log

--- tests/test_log.cc
+++ tests/test_log.cc
@@ -1,7 +1,17 @@
 #include <iostream>
-#include "src/log.h"
+#include <string>
+#include "newboy/log.h"
 
 int main(int argc, char const *argv[]) {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello Newboy Logger!" << std::endl;
+
+    newboy::Logger::ptr logger(new newboy::Logger);
+
+    newboy::LogEvent::ptr event(new newboy::LogEvent(logger, newboy::LogLevel::DEBUG, 
+                                                     __FILE__, __LINE__, 0, rand(), rand(),
+                                                     time(0), std::string("test_log")));
+    event->getSS() << "hello sylar log";
+    logger->log(newboy::LogLevel::DEBUG, event);
+
     return 0;
 }
