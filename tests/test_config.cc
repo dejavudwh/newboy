c73500a637070c7205f8d73e90c8514e2c8f3b1d
dejavudwh <daweihucs@outlook.com>
2021-07-13 20:30:03 +0800

    Add YAML-CPP dependencies and tests

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -1,7 +1,14 @@
 #include "../newboy/config.h"
+#include <yaml-cpp/yaml.h>
 
 newboy::ConfigVar<int>::ptr g_int_value_config = 
     newboy::Config::Lookup("system.port", (int)8080, "system port");
+
+void test_yaml() {
+    YAML::Node root = YAML::LoadFile("../bin/conf/log.yml");
+
+    NEWBOY_LOG_INFO(NEWBOY_LOG_ROOT()) << root;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -10,5 +17,8 @@ int main(int argc, char const *argv[])
     // getSS没有汇集所有流
     // NEWBOY_LOG_INFO(l) << g_int_value_config->getValue() << std::endl;
     // NEWBOY_LOG_INFO(l) << g_int_value_config->toString() << std::endl;
+    
+    test_yaml();
+    
     return 0;
 }
